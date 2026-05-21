047a08e5008bfd2f839bfde0b9a35a7539f34674
BORNET Alban <Alban.Bornet@hug.ch>
2026-05-21 10:19:38 +0200

    increased memory util

--- experiments/experiment_curated.sh
+++ experiments/experiment_curated.sh
@@ -9,7 +9,7 @@ PROMPT_CONFIG_PATH="./configs/prompt_config.yaml"
 
 # Configuration
 INFERENCE_BACKEND="vllm-serve-async"
-GPU_MEM_UTIL="0.55"
+GPU_MEM_UTIL="0.90"
 MAX_CONCURRENT_INFS="64"
 
 # Check if the model configuration file exists
